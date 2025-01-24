31abd0295c4a55920990a05cd6731453643c09e5
zainab-ali <zainab@kebab-ca.se>
2025-01-24 19:27:08 +0000

    Rewrite incremental pipelines.

--- scala/Part1Introduction/Code3IncrementalPipelines.sc
+++ scala/Part1Introduction/Code3IncrementalPipelines.sc
@@ -1,26 +1,33 @@
 import fs2.*
 
-def mostCommonInList(numbers: List[Int]): Int = {
-  val initialCounts: Map[Int, Int] = Map.empty
-  val finalCounts = numbers.foldLeft(initialCounts)((counts, i) =>
-    counts + ((i, counts.getOrElse(i, 0) + 1))
-  )
-  finalCounts.maxBy(_._2)._1
+def mostCommonInList(ids: List[String]): String = {
+  val initialCounts: Map[String, Int] = Map.empty
+  val finalCounts = ids.foldLeft(initialCounts) { (counts, id) =>
+    val previousCount = counts.getOrElse(id, 0)
+    counts + ((id, previousCount + 1))
+  }
+  val (mostCommonId, _) = finalCounts.maxBy { case (_, count) => count }
+  mostCommonId
 }
 
-def mostCommon(numbers: Stream[Pure, Int]): Int = {
-  val initialCounts: Map[Int, Int] = Map.empty
-  val finalCounts = numbers
-    .fold(initialCounts)((counts, i) =>
-      counts + ((i, counts.getOrElse(i, 0) + 1))
-    )
+def mostCommonInStream(ids: Stream[Pure, String]): String = {
+  val initialCounts: Map[String, Int] = Map.empty
+  val finalCounts = ids
+    .fold(initialCounts) { (counts, id) =>
+      val previousCount = counts.getOrElse(id, 0)
+      counts + ((id, previousCount + 1))
+    }
     .compile
     .last
     .getOrElse(initialCounts)
-  finalCounts.maxBy(_._2)._1
+  val (mostCommonId, _) = finalCounts.maxBy { case (_, count) => count }
+  mostCommonId
 }
 
+val oneAndFortyTwo: Stream[Pure, String] =
+  Stream("user42", "user1", "user42").repeatN(Int.MaxValue / 100)
 
-val numbers: Stream[Pure, Int] = Stream.range(0, Int.MaxValue).take(Int.MaxValue / 100)
+mostCommonInStream(oneAndFortyTwo)
 
-val oneAndFortyTwo: Stream[Pure, Int] = Stream(42, 1, 42).repeatN(Int.MaxValue / 100)
+val userIds: Stream[Pure, String] =
+  Stream.range(0, Int.MaxValue).map(i => s"u$i").take(Int.MaxValue / 100)
--- scala/Part1Introduction/Ex2IncrementalPipelines.test.scala
+++ scala/Part1Introduction/Ex2IncrementalPipelines.test.scala
@@ -3,34 +3,40 @@ import fs2.*
 
 class Ex2IncrementalPipelines extends CatsEffectSuite {
 
-  val numbers: Stream[Pure, Int] = Stream.range(0, Int.MaxValue)
+  val userIds: Stream[Pure, String] =
+    Stream.range(0, Int.MaxValue).map(id => s"u$id")
 
-  val oneAndFortyTwo: Stream[Pure, Int] = Stream(42, 1, 42).repeatN(100)
+  val oneAndFortyTwo: Stream[Pure, String] =
+    Stream("u42", "u1", "u42").repeatN(100)
 
-  def mostCommon(k: Int, numbers: Stream[Pure, Int]): Int = {
-    val initialCounts: Map[Int, Int] = Map.empty
-    val finalCounts = numbers
-      .fold(initialCounts)((counts, i) =>
-        counts + ((i, counts.getOrElse(i, 0) + 1))
-      )
+  def mostCommon(k: Int, ids: Stream[Pure, String]): String = {
+    // Replace this implementation with the Misra Gries summary.
+    val initialCounts: Map[String, Int] = Map.empty
+    val finalCounts = ids
+      .fold(initialCounts) { (counts, id) =>
+        val previousCount = counts.getOrElse(id, 0)
+        counts + ((id, previousCount + 1))
+      }
       .compile
       .last
       .getOrElse(initialCounts)
-    finalCounts.maxBy(_._2)._1
+    val (mostCommonId, _) = finalCounts.maxBy { case (_, count) => count }
+    mostCommonId
   }
 
   test("The answer is forty two") {
-    assertEquals(mostCommon(k = 100, oneAndFortyTwo), 42)
+    assertEquals(mostCommon(k = 100, oneAndFortyTwo), "u42")
   }
 
-  test("The most common element is present") {
-    assert(clue(mostCommon(k = 100, numbers.tail)) != 0)
+  test("The mostCommon function can process a large volume of ids") {
+    val mostlyFortyTwo = Stream("u42").repeatN(100) ++ userIds
+    assert(clue(mostCommon(k = 100, mostlyFortyTwo)) == "u42")
   }
 
   test("the most common element among mostly forty twos is forty two") {
     // Construct your own test stream
-    val testData: Stream[Pure, Int] = ???
-    assertEquals(testData.take(6).toList, List(0, 42, 1, 42, 2, 42))
-    assertEquals(mostCommon(k = 100, testData), 42)
+    val testData: Stream[Pure, String] =
+      userIds.interleave(Stream("u42").repeat).take(Int.MaxValue)
+    assertEquals(mostCommon(k = 100, testData), "u42")
   }
 }
