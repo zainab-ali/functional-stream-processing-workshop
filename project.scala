bbb6176fbe25fe6e3d1b93574b07699e7d84f111
zainab-ali <zainab@kebab-ca.se>
2025-02-12 22:08:04 +0000

    Update README.

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
 //> using javaOpt -Xmx200m
-//> using scala "3.5.0"
+//> using scala "3.6.3"
 //> using dep "com.github.zainab-ali::aquascape::0.3.0"
 //> using test.dep org.typelevel::munit-cats-effect::2.0.0
 //> using test.dep org.typelevel::cats-effect-testkit::3.5.4
