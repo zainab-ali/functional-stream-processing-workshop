89c05a87f3aeabd4b5c1c41c74bb5f3898aa3af1
zainab-ali <zainab@kebab-ca.se>
2025-04-02 11:44:12 +0100

    Add solitions to exercises.

--- scala/Part1Introduction/Ex1WhatAreStreams.solutions.test.scala
+++ scala/Part1Introduction/Ex1WhatAreStreams.solutions.test.scala
@@ -1,32 +1,32 @@
 import munit.*
 import fs2.*
 
-class Ex1WhatAreStreams extends CatsEffectSuite {
+class Ex1WhatAreStreamsSolutions extends CatsEffectSuite {
 
   val numbers: Stream[Pure, Int] = Stream.range(0, Int.MaxValue)
 
   test("get the first two numbers") {
-    val result: List[Int] = ???
+    val result: List[Int] = numbers.take(2).toList
     assertEquals(result, List(0, 1))
   }
 
   test("get the seventh to the ninth number") {
-    val result: List[Int] = ???
+    val result: List[Int] = numbers.drop(6).take(3).toList
     assertEquals(result, List(6, 7, 8))
   }
 
   test("get odd numbers less than six") {
-    val result: List[Int] = ???
+    val result: List[Int] = numbers.filter(n => n % 2 == 1).take(3).toList
     assertEquals(result, List(1, 3, 5))
   }
 
   test("get the 1000th number") {
-    val result: Int = ???
+    val result: Int = numbers.drop(1000).head.compile.last.get
     assertEquals(result, 1000)
   }
 
   test("get the sum of the first 10 numbers") {
-    val result = ???
+    val result = numbers.take(10).compile.foldMonoid
     assertEquals(result, 45)
   }
 }
