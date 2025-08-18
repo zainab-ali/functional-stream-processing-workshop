eb514a8099102cbc429e3b268a5896454fd666cf
zainab-ali <zainab@kebab-ca.se>
2025-08-18 10:11:38 +0100

    Delete scalajs directory (multi-project build fails).

--- scalajs/WorkshopAquascapeApp.scala
+++
@@ -1,6 +0,0 @@
-import aquascape.*
-import aquascape.drawing.*
-trait WorkshopAquascapeApp extends AquascapeApp {
-  override def config: Config = super.config.scale(4)
-  override def name: String = "aquascape"
-}
